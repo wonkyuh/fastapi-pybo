82c1012bd590422dccf53761ab4f399b296fb90b
wonkyuh <wonkyuh@naver.com>
2025-01-09 15:51:05 +0900

    Add K8s Version

--- migrations/env.py
+++ migrations/env.py
@@ -20,6 +20,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 import models
 target_metadata = models.Base.metadata
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
